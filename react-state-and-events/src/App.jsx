6f2f64d45c1ca0d26b35a51bdd6e56279387a09b
lisifun <lisandrafj99@gmail.com>
2023-12-09 23:02:22 -0700

    lesson done

---
+++ react-state-and-events/src/App.jsx
@@ -0,0 +1,25 @@
+// App.jsx
+import { useState } from "react";
+import "./App.css";
+import Counter from "./components/Counter";
+
+function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = (event) => {
+    setTheme(event.target.value);
+  };
+
+  return (
+    <div className={"App " + theme}>
+      <h1>React - state and events</h1>
+      <Counter />
+
+      <select onChange={toggleTheme}>
+        <option value="light"> Light </option>
+        <option value="dark"> Dark </option>
+      </select>
+    </div>
+  );
+}
+export default App;
---
+++ react-state-and-events/src/components/Counter.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="Counter">
+      <h2>Counter</h2>
+
+      <p>You clicked {count} times!</p>
+
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        {" "}
+        -{" "}
+      </button>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        {" "}
+        +{" "}
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
